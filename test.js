efbd49a908b0fd7a72ef34c50ea605da201592c8
West <west@misfit.com>
2015-01-08 15:13:24 +0800

    testing

---
+++ test.js
@@ -0,0 +1,31 @@
+var should = require("should")
+
+describe('MyClass', function () {
+
+  describe('addOne()', function () {
+
+    it('should correctly add one to the given number', function () {
+      // assertions here
+    })
+
+  })
+
+  describe('subtractOne()', function () {
+
+    it('should correctly subtract one from the given number', function () {
+      // assertions here
+    })
+
+  })
+
+  describe('Student', function () {
+
+    var student = { classes: [ 'English', 'Maths', 'Science' ] }
+
+    it('should have correct number of classes', function () {
+      student.should.have.property('classes').with.lengthOf(3)
+    })
+
+  })
+
+})
