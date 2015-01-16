36b953160739c4bf40f0fbd4d92f78b11f9abba8
West <west@misfit.com>
2015-01-16 14:52:13 +0800

    alt message

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send(['Hello World!','Date 2015-01-16 0002',''].join(EOL));
+  res.send(['Hello World!','Date 2015-01-16 0003',''].join(EOL));
 });
 
 app.get('/ping', function (req, res) {
