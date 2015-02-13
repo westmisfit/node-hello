e27fbbed648a359805ddf0b4bf274d3d8f5ce87d
West Wang <west@misfit.com>
2015-02-13 11:09:43 +0800

    alert message

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send(['Hello World!','Date 2015-01-20',''].join(EOL));
+  res.send(['Hello World!',''].join(EOL));
 });
 
 var _request_count = 1;
