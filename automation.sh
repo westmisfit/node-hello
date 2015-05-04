8a09b7dcfee9ca074ccc2fd7a9ff5f0bc1760400
forhot2000 <wx1983@gmail.com>
2015-05-04 18:55:27 +0800

    add automation.sh

---
+++ automation.sh
@@ -0,0 +1,23 @@
+# this script file will run on misfit-cloud-builder server, don't run on local
+
+# set DOCKER_IMAGE will enable build docker image, and run integration tests on container
+export DOCKER_IMAGE="forhot2000/node-hello"
+
+# overwrite docker run command
+function docker_run () {
+	if [[ "$GIT_BRANCH" = "master" ]]; then
+    	CONTAINER_ID=$(docker run -d -e NODE_ENV=production ${DOCKER_IMAGE}${IMAGE_TAG})
+	else if [[ "$GIT_BRANCH" = "master" ]]; then
+    	CONTAINER_ID=$(docker run -d -e NODE_ENV=staging ${DOCKER_IMAGE}${IMAGE_TAG})
+	fi
+}
+
+# overwrite integreation test, this function will executed after docker container run
+function integration_test () {
+    check_url http://$CONTAINER_ID:8080/
+}
+
+# overwrite integreation test, this function will executed without docker container
+function integration_test_without_docker () {
+    echo "run integration test without docker..."
+}
