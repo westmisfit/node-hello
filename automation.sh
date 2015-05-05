b661cf1b60e6f7dfbd9dae1124d404dfa629a9b1
forhot2000 <wx1983@gmail.com>
2015-05-05 10:44:14 +0800

    [skip ci] format

--- automation.sh
+++ automation.sh
@@ -2,22 +2,19 @@
 
 # set DOCKER_IMAGE will enable build docker image, and run integration tests on container
 export DOCKER_IMAGE="forhot2000/node-hello"
+# set DOCKER_PUSH false will ignore docker push
+# export DOCKER_PUSH="false"
 
 # overwrite docker run command
 function docker_run () {
-	if [[ "$GIT_BRANCH" = "master" ]]; then
-    	CONTAINER_ID=$(docker run -d -e NODE_ENV=production ${DOCKER_IMAGE}${IMAGE_TAG})
-	elif [[ "$GIT_BRANCH" = "develop" ]]; then
-    	CONTAINER_ID=$(docker run -d -e NODE_ENV=staging ${DOCKER_IMAGE}${IMAGE_TAG})
-	fi
+    if [[ "$GIT_BRANCH" = "master" ]]; then
+        CONTAINER_ID=$(docker run -d -e NODE_ENV=production ${DOCKER_IMAGE}${IMAGE_TAG})
+    elif [[ "$GIT_BRANCH" = "develop" ]]; then
+        CONTAINER_ID=$(docker run -d -e NODE_ENV=staging ${DOCKER_IMAGE}${IMAGE_TAG})
+    fi
 }
 
 # overwrite integreation test, this function will executed after docker container run
 function integration_test () {
     check_url "http://${CONTAINER_IP}:8080/"
 }
-
-# overwrite integreation test, this function will executed without docker container
-# function integration_test_without_docker () {
-#     echo "run integration test without docker..."
-# }
